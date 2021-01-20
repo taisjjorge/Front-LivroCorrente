40b08eb000f14297bca034fda1b40fdf0740c5e2
Renata Lima <limarenataa@gmail.com>
2021-01-20 16:21:21 -0300

    Adicionando responsividade ao componente Footer e BtnCard

--- src/Components/Depoimento/index.js
+++ src/Components/Depoimento/index.js
@@ -9,7 +9,7 @@ export default function Depoimentos() {
                 <img className="img-banner d-block w-100"src="https://i.imgur.com/gitl40R.png" alt="First slide"/>
             </Carousel.Item>
             <Carousel.Item interval={3000} >
-                <img className="img-banner d-block w-100"src="https://i.imgur.com/OBwPQRb.png" alt="Second slide"/>
+                <img className="img-banner d-block w-100"src="https://i.imgur.com/gitl40R.png" alt="Second slide"/>
             </Carousel.Item>
         </Carousel >
     
